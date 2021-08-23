9f16842886307ccf5d31eabeee1befec50c8296c
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2021-08-23 15:54:33 -0400

    wrap apr in a try catch

--- src/routes/pangolin.ts
+++ src/routes/pangolin.ts
@@ -46,58 +46,62 @@ export const average: Handler = async function (_, response) {
 
 // GET /pangolin/apr/:address
 export const apr: Handler = async function (request, response) {
-  const stakingAddress = request.params.address;
+  try {
+    const stakingAddress = request.params.address;
 
-  if (!STAKING_ADDRESSES.includes(stakingAddress)) {
-    response.end('0');
-    return;
+    if (!STAKING_ADDRESSES.includes(stakingAddress)) {
+      response.end('0');
+      return;
+    }
+
+    const stakingTokenAddress = await getStakingTokenAddress(stakingAddress);
+
+    // How much PGL is staked
+    const poolTokenBalance = await getBalance(stakingTokenAddress, stakingAddress);
+
+    // Total PGL supply
+    const poolTokenSupply = await getTotalSupply(stakingTokenAddress);
+
+    // Get the two token addresses in the pool
+    const [token0, token1] = await getPoolTokens(stakingTokenAddress);
+
+    // Get how much AVAX and PNG are in the AVAX-PNG pool
+    const [pooledAVAX, pooledPNG] = await Promise.all([
+      await getBalance(WAVAX_ADDRESS, WAVAX_PNG_ADDRESS),
+      await getBalance(PNG_ADDRESS, WAVAX_PNG_ADDRESS),
+    ]);
+
+    const stakedAVAX = [token0, token1].includes(WAVAX_ADDRESS)
+      ? (await getBalance(WAVAX_ADDRESS, stakingTokenAddress))
+          // Other side of pool has equal value
+          .mul(2)
+          // Not all PGL is staked
+          .mul(poolTokenBalance)
+          .div(poolTokenSupply)
+      : (await getBalance(PNG_ADDRESS, stakingTokenAddress))
+          // Other side of pool has equal value
+          .mul(2)
+          // Convert to AVAX
+          .mul(pooledAVAX)
+          .div(pooledPNG)
+          // Not all PGL is staked
+          .mul(poolTokenBalance)
+          .div(poolTokenSupply);
+
+    const rewardRate = (await getRewardRate(stakingAddress))
+      // Reward rate is per second
+      .mul(60 * 60 * 24 * 7 * 52)
+      // Convert to AVAX
+      .mul(pooledAVAX)
+      .div(pooledPNG)
+      // Percentage
+      .mul(100)
+      // Divide by amount staked to get APR
+      .div(stakedAVAX);
+
+    response.setHeader('Cache-Control', 'public,s-maxage=30');
+    response.send(200, rewardRate.toString());
+  } catch {
+    response.send(200, 0);
   }
-
-  const stakingTokenAddress = await getStakingTokenAddress(stakingAddress);
-
-  // How much PGL is staked
-  const poolTokenBalance = await getBalance(stakingTokenAddress, stakingAddress);
-
-  // Total PGL supply
-  const poolTokenSupply = await getTotalSupply(stakingTokenAddress);
-
-  // Get the two token addresses in the pool
-  const [token0, token1] = await getPoolTokens(stakingTokenAddress);
-
-  // Get how much AVAX and PNG are in the AVAX-PNG pool
-  const [pooledAVAX, pooledPNG] = await Promise.all([
-    await getBalance(WAVAX_ADDRESS, WAVAX_PNG_ADDRESS),
-    await getBalance(PNG_ADDRESS, WAVAX_PNG_ADDRESS),
-  ]);
-
-  const stakedAVAX = [token0, token1].includes(WAVAX_ADDRESS)
-    ? (await getBalance(WAVAX_ADDRESS, stakingTokenAddress))
-        // Other side of pool has equal value
-        .mul(2)
-        // Not all PGL is staked
-        .mul(poolTokenBalance)
-        .div(poolTokenSupply)
-    : (await getBalance(PNG_ADDRESS, stakingTokenAddress))
-        // Other side of pool has equal value
-        .mul(2)
-        // Convert to AVAX
-        .mul(pooledAVAX)
-        .div(pooledPNG)
-        // Not all PGL is staked
-        .mul(poolTokenBalance)
-        .div(poolTokenSupply);
-
-  const rewardRate = (await getRewardRate(stakingAddress))
-    // Reward rate is per second
-    .mul(60 * 60 * 24 * 7 * 52)
-    // Convert to AVAX
-    .mul(pooledAVAX)
-    .div(pooledPNG)
-    // Percentage
-    .mul(100)
-    // Divide by amount staked to get APR
-    .div(stakedAVAX);
-
-  response.setHeader('Cache-Control', 'public,s-maxage=30');
-  response.send(200, rewardRate.toString());
 };
